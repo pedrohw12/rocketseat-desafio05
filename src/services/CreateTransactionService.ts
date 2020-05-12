796cd3b1c4d6217c404f8b6f2fbd139daea78fa3
Pedro Wanderley <peu-zico>
2020-05-12 17:35:42 -0300

    done

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import { uuid } from 'uuidv4';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
+  public execute({ title, value, type }: Request): Transaction {
     // TODO
+    const balance = this.transactionsRepository.getBalance();
+
+    if (balance.total < value && type === 'outcome') {
+      throw Error('Transaction bigger than the balance')
+    }
+
+    if (type === 'income') {
+      balance.income += value;
+    } else if (type === 'outcome') {
+      balance.outcome += value;
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    })
+    return transaction;
   }
 }
 
